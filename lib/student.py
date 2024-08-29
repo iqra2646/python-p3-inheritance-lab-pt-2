3a0c7bf6f2627e5ce35cabfdb6307362dbfe520e
iqra <iqralul9@gmail.com>
2024-08-29 14:42:34 +0300

    done assigment

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,25 @@
+# Define the Student class
 class Student:
-    pass
+    # Method to print a greeting message
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+    
+    # Method to print the phrase "Pick me!"
+    def raise_hand(self):
+        print("Pick me!")
 
+# Define the ChattyStudent class, which inherits from Student
 class ChattyStudent(Student):
-    pass
+    # Method that overrides the hello() method from Student
+    def hello(self):
+        # Call the hello() method from the superclass (Student)
+        super().hello()
+        # Print additional chatty messages
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? "
+              "You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    # Method that overrides the raise_hand() method from Student
+    def raise_hand(self):
+        # Call the raise_hand() method from the superclass (Student) 10 times
+        for _ in range(10):
+            super().raise_hand()
